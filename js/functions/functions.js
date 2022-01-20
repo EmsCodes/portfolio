3725be178ae283130a2360c2f85ee12dda5ce770
EmsCodes <69721898+EmsCodes@users.noreply.github.com>
2022-01-20 17:56:53 +0100

    nav & introduction section
    nav animation, styling etc. and intro styling

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -1,6 +1,12 @@
 const headerNav = document.querySelector("nav");
+const logo = document.querySelector(".logo");
 
 export function dropDownMenu() {
 	headerNav.classList.toggle("active");
-	console.log("lol");
+
+	if (headerNav.classList.contains("active")) {
+		logo.src = "images/Logo-dark-blue.png";
+	} else {
+		logo.src = "images/Logo-white.png";
+	}
 }
