a11c9dca187689b87dfdb494d5845f04a2dc2ec9
Endre Makra-Stenkløv <69721898+EmsCodes@users.noreply.github.com>
2022-06-18 00:30:08 +0200

    .

--- js/functions/displayOnScroll.js
+++ js/functions/displayOnScroll.js
@@ -1,16 +1,63 @@
+// const scrollElements = document.querySelectorAll(".js-scroll");
+
+// scrollElements.forEach((el) => {
+//   el.style.opacity = 0;
+// });
+
+// const elementInView = (el, ) => {
+//   const elementTop = el.getBoundingClientRect().top;
+
+//   console.log(elementTop);
+
+//   return (
+//     elementTop <=
+//     (window.innerHeight || document.documentElement.clientHeight) / dividend
+//   );
+// };
+
+// const elementOutofView = (el) => {
+//   const elementTop = el.getBoundingClientRect().top;
+
+//   return (
+//     elementTop > (window.innerHeight || document.documentElement.clientHeight)
+//   );
+// };
+
+// const displayScrollElement = (element) => {
+//   element.classList.add("scrolled");
+// };
+
+// const hideScrollElement = (element) => {
+//   element.classList.remove("scrolled");
+// };
+
+// const handleScrollAnimation = () => {
+//   scrollElements.forEach((el) => {
+//     if (elementInView(el, 0.5)) {
+//       displayScrollElement(el);
+//     } else if (elementOutofView(el)) {
+//       hideScrollElement(el);
+//     }
+//   });
+// };
+// window.addEventListener("scroll", handleScrollAnimation);
+
 const scrollElements = document.querySelectorAll(".js-scroll");
 
-scrollElements.forEach((el) => {
-  el.style.opacity = 0;
-});
-
-const elementInView = (el, percentageScroll = 100) => {
+const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
 
   return (
     elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) *
-      (percentageScroll / 100)
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
   );
 };
 
@@ -24,9 +71,9 @@ const hideScrollElement = (element) => {
 
 export const handleScrollAnimation = () => {
   scrollElements.forEach((el) => {
-    if (elementInView(el, 100)) {
+    if (elementInView(el, 1.5)) {
       displayScrollElement(el);
-    } else {
+    } else if (elementOutofView(el)) {
       hideScrollElement(el);
     }
   });
